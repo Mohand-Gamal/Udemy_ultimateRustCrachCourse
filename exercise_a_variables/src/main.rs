b447f11abe924fb0936c54808736906d2dd1608a
Mohanad <mohandgamal1520@gmail.com>
2023-03-11 21:58:56 +0200

    Added code for Exercise E Ownership & References, in additions to fixings warnings in code of Exercise A Variables

--- exercise_a_variables/src/main.rs
+++ exercise_a_variables/src/main.rs
@@ -2,8 +2,8 @@ const STARTING_MISSILES: i32 = 8;
 const READY_AMOUNT: i32 = 2;
 
 fn main() {
-    let (mut missiles, ready): (i32 , i32)= (STARTING_MISSILES, READY_AMOUNT);
-    let extraVariable: i32 = 0;
+    let (missiles, ready): (i32 , i32)= (STARTING_MISSILES, READY_AMOUNT);
+    // let extraVariable: i32 = 0;
     println!("Firing {} of my {} missiles...",ready, missiles);
     println!("{} missiles left", missiles - ready);
     // READY_AMOUNT = 1;
